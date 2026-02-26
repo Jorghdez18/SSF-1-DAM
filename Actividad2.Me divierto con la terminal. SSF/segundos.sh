34aa7cdfa6ebb622b325808e79624792684f1530
Jorge Hernández Pérez <jorcarherpe@gmail.com>
2026-02-26 15:07:13 +0000

    Update segundos.sh

--- Actividad2.Me divierto con la terminal. SSF/segundos.sh
+++ Actividad2.Me divierto con la terminal. SSF/segundos.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+#Jorge Hernádez Pérez 1ºDAM
 
+echo "Introduce dias: "
+read dias
+
+echo "Introduce horas: "
+read horas
+
+echo "Introduce segundos: "
+read segundos
+
+total=$((dias*86400 + horas*3600 +segundos))
+echo "Total de segundos: $total"
