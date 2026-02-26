05f56c64c12e276246b1a7964b3991c41f26ec2a
Jorge Hernández Pérez <jorcarherpe@gmail.com>
2026-02-26 15:22:55 +0000

    Create imc.sh

---
+++ Actividad2.Me divierto con la terminal. SSF/imc.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+#Jorge Hernández Pérez
+
+echo "Introduce altura en cm:"
+read altura
+
+echo "Introduce peso en kg:"
+read peso
+
+altura_m=$(echo "$altura / 100" | bc -l)
+
+imc=$(echo "$peso / ($altura_m * $altura_m)" | bc -l)
+
+echo "IMC: $imc"
+
+if (( $(echo "$imc < 18.5" | bc -l) ))
+then
+echo "Bajo peso"
+else
+if (( $(echo "$imc < 25" | bc -l) ))
+then
+echo "Peso normal"
+else
+if (( $(echo "$imc < 30" | bc -l) ))
+then
+echo "Sobrepeso"
+else
+echo "Obesidad"
+fi 
+fi
+fi
+
