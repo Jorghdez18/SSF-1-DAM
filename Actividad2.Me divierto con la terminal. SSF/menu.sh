9555584ad2350bd3fd0f7dd11ae9e9e4a2979c89
Jorge Hernández Pérez <jorcarherpe@gmail.com>
2026-02-26 19:28:45 +0000

    Update menu.sh

--- Actividad2.Me divierto con la terminal. SSF/menu.sh
+++ Actividad2.Me divierto con la terminal. SSF/menu.sh
@@ -4,3 +4,10 @@
 #Script que muestra un menú, con diferentes datos de la maquina
 =================================================================
 
+echo "1. Espacio libre en disco (%)"
+echo "2. Espacio libre (tamaño)"
+echo "3. Usuario actual y nombre de la máquina"
+echo "4. Números de usuarios en la máquina (/etc/passwd)"
+echo "6. Espacio usado en tu carpeta"
+echo
+echo "Elija una opción: "
