d5c1d683e80f3ca9f45f87b21542a4267280e172
Jorge Hernández Pérez <jorcarherpe@gmail.com>
2026-02-26 19:20:10 +0000

    Create menu.sh

---
+++ Actividad2.Me divierto con la terminal. SSF/menu.sh
@@ -0,0 +1,6 @@
+=================================================================
+#!/usr/bin/bash
+#Jorge Hernández Pérez
+#Script que muestra un menú, con diferentes datos de la maquina
+=================================================================
+
